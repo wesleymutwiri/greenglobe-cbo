95cb51f346c4180bacb5278b657fa92819a447be
wesleymutwiri <wesleymochiemo@gmail.com>
2020-08-05 12:01:21 +0300

    commented out light theme option

--- src/components/themes/themes.js
+++ src/components/themes/themes.js
@@ -1,16 +1,18 @@
 export const presets = [
-  {
-    name: "light",
-    colors: {
-      text: "#060705",
-      background: "#f1f1f1",
-    },
-  },
+  // {
+  //   name: "light",
+  //   colors: {
+  //     text: "#060705",
+  //     background: "#f1f1f1",
+  //     svg: "#1e1e1e",
+  //   },
+  // },
   {
     name: "dark",
     colors: {
       text: "#f1f1f1",
       background: "#060705",
+      svg: "#1e1e1e",
     },
   },
 ];
