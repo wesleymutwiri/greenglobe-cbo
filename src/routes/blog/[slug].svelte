93510323f7779a469ff6da5273dc6cb2d8161207
wesleymutwiri <wesleymochiemo@gmail.com>
2020-08-06 20:35:21 +0300

    added blog backend link

--- src/routes/blog/[slug].svelte
+++ src/routes/blog/[slug].svelte
@@ -1,64 +1,21 @@
 <script context="module">
-	export async function preload({ params, query }) {
-		// the `slug` parameter is available because
-		// this file is called [slug].svelte
-		const res = await this.fetch(`blog/${params.slug}.json`);
-		const data = await res.json();
+  const apiUrl = process.env.SAPPER_APP_API_URL;
 
-		if (res.status === 200) {
-			return { post: data };
-		} else {
-			this.error(res.status, data.message);
-		}
-	}
+  export async function preload({ params, query }) {
+    const res = await this.fetch(`${apiUrl}/wp/v2/posts?slug=${params.slug}`);
+    const data = await res.json();
+
+    return { post: data[0] };
+  }
 </script>
 
 <script>
-	export let post;
+  export let post;
+  console.log(post);
 </script>
 
-<style>
-	/*
-		By default, CSS is locally scoped to the component,
-		and any unused styles are dead-code-eliminated.
-		In this page, Svelte can't know which elements are
-		going to appear inside the {{{post.html}}} block,
-		so we have to use the :global(...) modifier to target
-		all elements inside .content
-	*/
-	.content :global(h2) {
-		font-size: 1.4em;
-		font-weight: 500;
-	}
+<h1>{post.title.rendered}</h1>
 
-	.content :global(pre) {
-		background-color: #f9f9f9;
-		box-shadow: inset 1px 1px 5px rgba(0,0,0,0.05);
-		padding: 0.5em;
-		border-radius: 2px;
-		overflow-x: auto;
-	}
-
-	.content :global(pre) :global(code) {
-		background-color: transparent;
-		padding: 0;
-	}
-
-	.content :global(ul) {
-		line-height: 1.5;
-	}
-
-	.content :global(li) {
-		margin: 0 0 0.5em 0;
-	}
-</style>
-
-<svelte:head>
-	<title>{post.title}</title>
-</svelte:head>
-
-<h1>{post.title}</h1>
-
-<div class='content'>
-	{@html post.html}
+<div class="content">
+  {@html post.content.rendered}
 </div>
