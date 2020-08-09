ac8286420a3f755c67e0a616b46b166839a9f4ea
wesleymutwiri <wesleymochiemo@gmail.com>
2020-08-09 08:58:45 +0300

    added a bit of content to the about page

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -1,7 +1,156 @@
 <svelte:head>
-	<title>About</title>
+  <title>About</title>
 </svelte:head>
 
-<h1>About this site</h1>
+<div class="container">
+  <h1>About Us</h1>
+  <p>
+    Green Globe CBO is a grass-root Not-for-profit Community Based Organization
+    (CBO) based in Nairobi, Kenya. We aim to have clean and sustainable
+    communities around Nairobi City. Green Globe CBO is committed to educate
+    communities on proper waste disposal methods with specific emphasis on
+    plastic waste.
+  </p>
+  <p>
+    Green Globe CBO was formed by like-minded youths residing in Nairobi Kenya
+    who were concerned with the plethora of problems caused by plastic waste
+    when it leaks to the environment. Most of the plastic waste in Kenya ends up
+    in dumps or landfills where it takes many years to decompose. Plastic waste
+    has often clogged sewers and therefore providing a breeding ground for
+    mosquitoes and pests and in return increasing the risk of spread of vector
+    bone diseases. Green Globe CBO offers a holistic approach to beating plastic
+    pollution by training, providing incentives and economically empowering
+    youth and women groups to recycle, reuse and upcycle plastic waste.
+  </p>
 
-<p>This is the 'about' page. There's not much here.</p>
+  <h3>Programs & Projects</h3>
+  <p>
+    Our programs are geared towards creating awareness to change consumer habits
+    on use and disposal of plastics and are towards achieving the following
+    Sustainable Development Goals:
+  </p>
+  <ol>
+    <li>SDG 6: Clean water and Sanitation.</li>
+    <li>SDG 11: Sustainable Cities and Communities.</li>
+    <li>SDG 13: Climate Action.</li>
+  </ol>
+
+  <h3>SDG #6: Clean Water and Sanitation</h3>
+  <p>
+    40% of Kenyans still lack access to safe drinking water while 70% lack
+    access to basic sanitation solutions. Green Globe CBO is committed to
+    protect clean sources such as rivers from plastic waste by organizing river
+    clean-ups, unclogging drainages, and sewers to remove plastic waste,
+    advocating for river basin protection and restoration.
+  </p>
+
+  <h3>SDG#11: Sustainable Cities and Communities</h3>
+  <p>
+    Green Globe CBO advocates for a cleaner and greener Nairobi. We offer free
+    training on improved waste management practices such as sorting of waste at
+    household level, recycling and up-cycling. We have also established programs
+    and forums for youth and women groups trainings on how to recycle plastic to
+    make art and earn a living.
+  </p>
+
+  <h3>SDG#3 Climate Action</h3>
+  <p>
+    Climate change is exacerbating the intensity of extreme weather events such
+    as droughts, flooding and heat waves. Green Globe CBO creates awareness on
+    climate change, environmental conservations, protection of water bodies and
+    forests in Kenya. We promote public participation in issues related to
+    environmental protection, renewable energy, conservation and sustainable
+    development.
+  </p>
+
+  <h3>Ongoing Projects:</h3>
+  <p>
+    Green Globe CBO in partnership with other stakeholders facilitate diverse
+    activities with an aim to beat plastic pollution in Kenya. Our current
+    programs include:
+  </p>
+
+  <ol>
+    <h5><li>Youth & Women Group Trainings</li></h5>
+    <p>
+      Green Globe CBO offers free training to youth and women groups on a
+      variety of methods they can earn a living by recycling plastic waste. Our
+      trainings are open to ANY registered youth/women group in Kenya.
+    </p>
+    <p>
+      In our Trainings we educate the youth to not only be aware of the dangers
+      posed by plastic pollution but also to offer solutions to the plastic
+      pollution menace. We equip our trainees with skills to utilize poorly
+      disposed waste that would end up in landfills, sewers, rivers or other
+      water bodies.
+    </p>
+    <h5>
+      <li>Environment Education – Plastic Pollution</li>
+    </h5>
+    <p>
+      As part of our efforts to raise awareness about plastic pollution, Green
+      Globe CBO continually engage students from high schools and colleges and
+      educate them on climate change and environmental conservation.
+    </p>
+    <p>
+      By educating school going children Green Globe CBO hopes to inspire the
+      youth to come up with solutions to the challenges of poor plastic waste
+      disposal. We hope to create a generation that is environmental cautious.
+    </p>
+    <h5>
+      <li>Community Trainings – Changing Consumer Behavior</li>
+    </h5>
+    <p>
+      We believe that the most effective solution to plastic pollution is
+      changing consumer habits in households, as a result Green Globe CBO
+      embarks on regular community trainings in community centers, markets
+      centers and door to door to educate the general public on proper waste
+      disposal, ways to recycle, re-use and upcycle plastic waste.
+    </p>
+    <h5>
+      <li>Clean- Up Activities</li>
+    </h5>
+    <p>
+      We organize and facilitate clean-up activities within Nairobi County. Our
+      clean-ups involve removing plastic waste from clogged sewers, and from
+      streams and rivers. All the plastic waste collected in our clean ups is
+      recycled.
+    </p>
+    <h5>
+      <li>Backyard/Balcony Farming</li>
+    </h5>
+    <p>
+      Green Globe CBO is supporting vulnerable groups to start backyard/
+      balcony/rooftop gardens using plastic bottles planters. We provide
+      planters, red soil and seedlings to vulnerable groups such as children
+      home, elderly homes and people living with disabilities in Nairobi. This
+      project is aimed at reducing the cost of a meal for families that continue
+      to suffer the adverse effects of Covid-19. We also aim to ease the access
+      to nutritious and organic foods amid the Covid -19 pandemic
+    </p>
+  </ol>
+</div>
+<style>
+  .container {
+    padding: 20px;
+    margin: 40px;
+    max-width: 100vw;
+    line-height: 1.5;
+  }
+  .container > * {
+    padding: 10px;
+  }
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6 {
+    font-weight: 900;
+    font-size: larger;
+    color: #fed121;
+  }
+  ol {
+    margin-left: 3rem;
+  }
+</style>
