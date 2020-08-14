feb2a9ff54b18a82e3f649cec134390802c53b4f
wesleymutwiri <wesleymochiemo@gmail.com>
2020-08-14 14:46:31 +0300

    added twitter embed of the greenglobe site

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -6,7 +6,17 @@
 </svelte:head>
 <div class="container">
   <Carousel/>
+  <div class="fill-width">
+    <a class="twitter-timeline" data-align="center" data-theme="dark" data-height="1000" href="https://twitter.com/GreenGlobe_CBO?ref_src=twsrc%5Etfw">Tweets by GreenGlobe_CBO</a> 
+    <a href="https://twitter.com/GreenGlobe_CBO?ref_src=twsrc%5Etfw" class="twitter-follow-button" data-size="large" data-show-count="false">Follow @GreenGlobe_CBO</a>
+  </div>
 </div>
 <style>
- 
+.fill-width {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-items: center;
+  text-align: center;
+}
 </style>
