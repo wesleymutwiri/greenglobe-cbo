c09efe356af5c6541d95765125a1469335304a9e
wesleymutwiri <wesleymochiemo@gmail.com>
2020-10-05 22:50:53 +0300

    improving blog url to use markdown files

--- src/routes/blog/[slug].svelte
+++ src/routes/blog/[slug].svelte
@@ -1,21 +1,31 @@
 <script context="module">
-  const apiUrl = process.env.SAPPER_APP_API_URL;
-
   export async function preload({ params, query }) {
-    const res = await this.fetch(`${apiUrl}/wp/v2/posts?slug=${params.slug}`);
-    const data = await res.json();
-
-    return { post: data[0] };
+    const res = await this.fetch(`_posts/${params.slug}.md`);
+    if (res.status === 200) {
+      return { postMd: await res.text() };
+    } else {
+      this.error(res.status, data.message);
+    }
   }
 </script>
 
 <script>
-  export let post;
+  import fm from "front-matter";
+  import MarkdownIt from "markdown-it";
+  export let postMd;
+  const md = new MarkdownIt();
+  $: frontMatter = fm(postMd);
+  $: post = {
+    ...frontMatter.attributes,
+    html: md.render(frontMatter.body),
+  };
   console.log(post);
 </script>
-
+<svelte:head>
+  <title>{post.title.rendered}</title>
+</svelte:head>
 <h1>{post.title.rendered}</h1>
 
-<div class="content">
-  {@html post.content.rendered}
-</div>
+<div class="content">{@html post.content.rendered}</div>
+
+<style></style>
