33d4c44afa5dc1d03049ceb40806fcc7920d73ae
wesleymutwiri <wesleymochiemo@gmail.com>
2020-10-06 21:43:01 +0300

    fixing url issue in svelte

--- src/routes/blog/[slug].svelte
+++ src/routes/blog/[slug].svelte
@@ -1,6 +1,9 @@
 <script context="module">
   export async function preload({ params, query }) {
+    // the `slug` parameter is available because
+    // this file is called [slug].svelte
     const res = await this.fetch(`_posts/${params.slug}.md`);
+
     if (res.status === 200) {
       return { postMd: await res.text() };
     } else {
@@ -12,20 +15,14 @@
 <script>
   import fm from "front-matter";
   import MarkdownIt from "markdown-it";
+
   export let postMd;
+
   const md = new MarkdownIt();
+
   $: frontMatter = fm(postMd);
   $: post = {
     ...frontMatter.attributes,
     html: md.render(frontMatter.body),
   };
-  // console.log(post);
 </script>
-<svelte:head>
-  <title>{post.title.rendered}</title>
-</svelte:head>
-<h1>{post.title.rendered}</h1>
-
-<div class="content">{@html post.content.rendered}</div>
-
-<style></style>
